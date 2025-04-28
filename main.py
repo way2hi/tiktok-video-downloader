5aaf8fc0d4c5bf279b421a3b47a85ef96f487d4b
ashe <karlokuhada3@gmail.com>
2025-04-28 04:03:50 +0200

    Add files via upload

--- main.py
+++ main.py
@@ -1,68 +1,101 @@
-import io
-import sys
-import rich
-import time
-from rich import print
-from rich.console import Console
+import time, re, os, requests, browser_cookie3, json, sys
+from bs4 import BeautifulSoup
 from urllib.parse import urlparse
 
-console = Console()
+global cookies
+cookies = browser_cookie3.firefox()
 
-old_stdout = sys.stdout
-sys.stdout = io.StringIO()
+url_regex = '(?<=\.com/)(.+?)(?=\?|$)'
+video_id_regex = '(?<=/video/)([0-9]+)'
 
-import pyktok as pyk
-
-sys.stdout = old_stdout
+headers = {'Accept-Encoding': 'gzip, deflate, sdch',
+           'Accept-Language': 'en-US,en;q=0.8',
+           'Upgrade-Insecure-Requests': '1',
+           'User-Agent': 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/56.0.2924.87 Safari/537.36',
+           'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,*/*;q=0.8',
+           'Cache-Control': 'max-age=0',
+           'Connection': 'keep-alive'}
 
 def is_tiktok_url(url):
     parsed_url = urlparse(url)
     return parsed_url.hostname == 'www.tiktok.com' or parsed_url.hostname == 'tiktok.com' or parsed_url.hostname == 'm.tiktok.com'
 
-console.clear()
+def get_tiktok_json(video_url, browser_name=None):
+    global cookies
+    if browser_name is not None:
+        cookies = getattr(browser_cookie3, browser_name)(domain_name=".tiktok.com")
+    tt = requests.get(video_url,
+                      headers=headers,
+                      cookies=cookies,
+                      timeout=20)
+    cookies = tt.cookies
+    soup = BeautifulSoup(tt.text, "html.parser")
+    tt_script = soup.find('script', attrs={'id':"__UNIVERSAL_DATA_FOR_REHYDRATION__"})
+    try:
+        tt_json = json.loads(tt_script.string)
+    except AttributeError:
+        return
+    return tt_json
 
-console.print(f"""\n\n
-             ⣀⣀⣀⣀⣀⡀⠀⠀⠀⠀⠀⠀
-⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢰⣿⡿⠿⠿⢿⣿⠀⠀⠀⠀⠀⠀
-⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢸⣿⡇⠀⠀⠘⣿⣆⠀⠀⠀⠀⠀
-⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢸⣿⡇⠀⠀⠀⠘⢿⣷⣄⣀⠀⠀
-⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢸⣿⡇⠀⠀⠀⠀⠀⠙⠛⠿⢿⣿                        [bold deep_sky_blue3]TIKTOK VIDEO DOWNLOADER[/bold deep_sky_blue3]
-⠀⠀⠀⠀⠀⠀⠀⠀⣀⣀⠀⠀⢸⣿⡇⠀⠀⣴⣦⣄⡀⠀⠀⢸⣿                           [bold deep_sky_blue3]GITHUB.COM/RIPSAKU[/bold deep_sky_blue3]
-⠀⠀⠀⢀⣠⣶⣾⠿⢿⣿⡇⠀⢸⣿⡇⠀⠀⣿⣿⠻⠿⣿⣶⣾⣿
-⠀⠀⣰⣿⠟⠉⠀⠀⢸⣿⡇⠀⢸⣿⡇⠀⠀⣿⣿⠀⠀⠀⠀⠀⠁
-⠀⣾⡿⠃⠀⠀⣠⣴⣾⣿⠇⠀⢸⣿⡇⠀⠀⣿⣿⠀⠀⠀⠀⠀⠀
-⢸⣿⠃⠀⠀⣼⡿⠋⠀⠀⠀⠀⢸⣿⡇⠀⠀⣿⣿⠀⠀⠀⠀⠀⠀
-⣿⣿⠀⠀⢸⣿⡇⠀⠀⠀⠀⠀⢸⣿⡇⠀⠀⣿⣿⠀⠀⠀⠀⠀⠀
-⢸⣿⡀⠀⠀⢿⣷⡀⠀⠀⠀⢀⣼⣿⠁⠀⠀⣿⡟⠀⠀⠀⠀⠀⠀
-⠈⢿⣧⡀⠀⠈⠻⢿⣷⣶⣾⡿⠟⠁⠀⠀⣼⡿⠃⠀⠀⠀⠀⠀⠀
-⠀⠈⢻⣷⣄⡀⠀⠀⠀⠀⠀⠀⠀⠀⣠⣾⡿⠁⠀⠀⠀⠀⠀⠀⠀
-⠀⠀⠀⠉⠻⢿⣷⣦⣤⣤⣤⣴⣶⡿⠟⠋⠀⠀⠀⠀⠀⠀⠀⠀⠀
-⠀⠀⠀⠀⠀⠀⠈⠉⠉⠛⠋⠉⠁⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
-""", style="purple")
+def save(video_url, save_video=False, browser_name="firefox"):
+    if not save_video:
+        return
+    tt_json = get_tiktok_json(video_url, browser_name)
+    def download_file(url, filename):
+        headers['referer'] = 'https://www.tiktok.com/'
+        response = requests.get(url, allow_redirects=True, headers=headers, cookies=cookies)
+        with open(filename, "wb") as f:
+            f.write(response.content)
+    if not tt_json:
+        print("Failed to get tiktok data")
+        return
+    try:
+        video_id = list(tt_json['ItemModule'].keys())[0]
+        data_slot = tt_json['ItemModule'][video_id]
+    except (KeyError, IndexError):
+        data_slot = tt_json["__DEFAULT_SCOPE__"]['webapp.video-detail']['itemInfo']['itemStruct']
 
-console.print(f"[bold royal_blue1]Video link: [/bold royal_blue1]", end="")
+    base_filename = re.findall(url_regex, video_url)[0].replace('/', '_')
+
+    if save_video:
+        if 'imagePost' in data_slot:
+            for idx, slide in enumerate(data_slot['imagePost']['images'], start=1):
+                slide_filename = f"{base_filename}_slide_{idx}.jpeg"
+                download_file(slide['imageURL']['urlList'][0], slide_filename)
+        else:
+            try:
+                video_url_to_download = data_slot['video']['playAddr']
+                #print(data_slot['video'])
+            except KeyError:
+                video_url_to_download = data_slot['video']['playAddr']
+            
+            video_filename = f"{base_filename}.mp4"
+            download_file(video_url_to_download, video_filename)
+            print("Saved video to", os.getcwd())
+
+print("Enter the URL of the video you want to download: ")
 video_url = input()
 
 if not video_url:
-    console.print("[bold red]Error:[/bold red] URL cannot be empty.")
-    time.sleep(2.4)
+    print("URL cannot be empty")
+    time.sleep(2)
     exit()
 else:
     if not video_url.startswith(('http://', 'https://')):
         video_url = f"https://{video_url}"
-
     if not is_tiktok_url(video_url):
-        console.print(f"[bold red]Error:[/bold red] '{video_url}' is not a valid TikTok URL.")
-        time.sleep(2.4)
+        print(f"{video_url} is not a valid tiktok URL")
+        time.sleep(2)
         exit()
     else:
         try:
-            pyk.save_tiktok(video_url, True, '')
-            console.print("[bold green]Download completed![/bold green]")
+            save(video_url, True, "firefox")
+            print("Downloaded")
+            time.sleep(1)
         except Exception as e:
-            console.print(f"[bold red]Download error:[/bold red] {e}")
-            console.print("[italic yellow]The video might be private, deleted, or the URL is incorrect.[/italic yellow]")
-            time.sleep(2.4)
+            print(f"Err: {e} | Line: {sys.exc_info()[-1].tb_lineno}")
+            time.sleep(2)
             exit()
 
-#github.com/ripsaku
+# originally made by https://github.com/ripsaku
+# remade by yours truly :3
